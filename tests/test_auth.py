6e2a53f6697bd52ef673a5dfb8037c88d3e904d0
Anvar Kasimov <kasimov.ar@phystech.edu>
2025-05-12 03:15:40 +0300

    Tests really fixed

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -38,22 +38,36 @@ def test_me():
     assert response.status_code == 200
     assert response.json() == client.new_user_id
 
+def test_get_projects(): 
+    response = client.get("/projects")
+    assert response.status_code == 200
 
+def test_create_project():
+    response = client.post("/projects", headers={"Authorization": f"Bearer {client.auth_token}"},
+                           json={"project_description": fake.street_address(),
+                                 "project_name": client.fake_user_name
+                            }
+    )
+    assert response.status_code == 201
+    global project_id
+    project_id = response.json()["project_id"] 
+
+def test_update_project_by_id():
+    response = client.patch(f"/projects/{project_id}", headers={"Authorization": f"Bearer {client.auth_token}"}, json={"project_description": fake.street_address()})
+    assert response.status_code == 200
 
 def test_create_task():
     response = client.post("/tasks", headers={"Authorization": f"Bearer {client.auth_token}"},
                            json={"task_description": fake.street_address(),
                                  "due_date": str(fake.date_between(date.today(), timedelta(days=30))),
-                                 "assignee": 100,
+                                 "assignee": 1,
                                  "task_status": "open",
-                                 "project": 100
+                                 "project": project_id
                             }
     )
-    assert response.status_code == 422
-
-def test_get_projects():
-    response = client.get("/projects")
-    assert response.status_code == 200
+    assert response.status_code == 201
+    global task_id
+    task_id = response.json()["task_id"]
 
 def test_get_tasks():
     response = client.get("/tasks")
@@ -61,12 +75,24 @@ def test_get_tasks():
 
 def test_get_task_by_id():
     response = client.get(f"/tasks/{task_id}")
-    assert response.status_code == 201
+    assert response.status_code == 200
 
 def test_update_task():
-    response = client.patch(f"/tasks/{task_id}", json={"description": fake.street_address()})
-    assert response.status_code == 204
+    response = client.patch(f"/tasks/{task_id}", headers={"Authorization": f"Bearer {client.auth_token}"}, json={"task_description": fake.street_address()})
+    assert response.status_code == 200
 
 def test_delete_task():
     response = client.delete(f"/tasks/{task_id}")
-    assert response.status_code == 204
+    assert response.status_code == 200 
+
+def test_get_project_statistics():
+    response = client.get(f"/projects/{project_id}/statistics")
+    assert response.status_code == 200
+
+def test_get_latest_stats_snapshot():
+    response = client.get(f"/projects/{project_id}/statistics/latest?snapshot_date=" + str(date.today()))
+    assert response.status_code == 200
+
+def test_get_task_delta_in_period():
+    response = client.get(f"/projects/{project_id}/task_delta?start_date=2024-05-20&end_date=2024-05-21")
+    assert response.status_code == 200
